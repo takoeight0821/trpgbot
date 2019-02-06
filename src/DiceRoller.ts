6bf6d0aab9bdde64101ba4250e9121ae85aea89f
Yuya Kono <takohati0821@gmail.com>
2019-02-06 17:54:02 +0900

    update

--- src/DiceRoller.ts
+++ src/DiceRoller.ts
@@ -1,12 +1,15 @@
 import Discord = require('discord.js');
 import { getRandomInt } from './Utils';
+import * as math from 'mathjs';
 
 export class DiceRoller {
     msg: Discord.Message;
     text: string;
+    channel: Discord.Channel;
     constructor(msg: Discord.Message) {
         this.msg = msg;
         this.text = "";
+        this.channel = msg.channel;
     }
 
     addText(text: string, sep = "\n") {
@@ -22,7 +25,18 @@ export class DiceRoller {
 
     sendText() {
         if (this.text.length > 0) {
+            console.log("--- SEND start ---");
             this.msg.channel.send(this.text);
+
+            if (this.msg.channel instanceof Discord.DMChannel) {
+                console.log(this.msg.author.username + this.text);
+            } else if (this.msg.channel instanceof Discord.GroupDMChannel) {
+                console.log(this.msg.channel.name + this.text);
+            } else {
+                console.log(this.msg.channel.guild.name + this.text);
+            }
+
+            console.log("--- SEND end ---");
         }
     }
 
@@ -30,8 +44,36 @@ export class DiceRoller {
         const dices = diceRoll(count, roll);
         this.addText(`[${dices}`);
         this.trimText();
-        this.addText(`${dices.reduce((a, b) => a + b)}`, "] = ");
-        return dices.reduce((a, b) => a + b);
+        this.addText(`${dices.reduce((a, b) => a + b, 0)}`, "] = ");
+        return dices.reduce((a, b) => a + b, 0);
+    }
+
+    roll_nBn(count: number, roll: number, mode: string, limit: number) {
+        const dices = diceRoll(count, roll);
+
+        this.addText(`[${dices}`);
+        this.trimText();
+
+        let result = 0;
+        switch (mode) {
+            case ">=":
+                result = dices.filter(a => a >= limit).length;
+                break;
+            case "<=":
+                result = dices.filter(a => a <= limit).length;
+                break;
+            case ">":
+                result = dices.filter(a => a > limit).length;
+                break;
+            case "<":
+                result = dices.filter(a => a < limit).length;
+                break;
+            default:
+                this.addText("]", "");
+                return result;
+        }
+        this.addText(`${result}`, "] = ");
+        return result;
     }
 
     roll_dx(count: number, critical: number) {
@@ -54,18 +96,26 @@ export class DiceRoller {
     // this.msg.contentをパースし、フォーマットに従ってダイスロールを試みる。
     // 失敗した場合、falseを返す。
     roll() {
-        const nDn_re = /^(\d+)D(\d+)(\+\d+|\-\d+)?$/i;
-        const ndx_re = /^(\d+)DX(@\d+)?(\+\d+|\-\d+)?$/i;
+        const nDn_re = /^(\d+)D(\d+)(\+.+)?$/i;
+        const ndx_re = /^(\d+)DX(@\d+)?(\+.+)?$/i;
+        const nBn_re = /^(\d+)B(\d+)(?:(>=|<=|>|<)(\d+))?$/i;
         const D66_re = /^D66$/;
 
         const nDn = nDn_re.exec(this.msg.content);
-        const ndx = ndx_re.exec(this.msg.content)
+        const ndx = ndx_re.exec(this.msg.content);
+        const nBn = nBn_re.exec(this.msg.content);
 
         if (nDn !== null) {
-            this.rollAndCalc(nDn[0], this.roll_nDn(Number(nDn[1]), Number(nDn[2])), nDn[3]);
+            this.addText("roll " + nDn[0]);
+            this.calcCorrection(this.roll_nDn(Number(nDn[1]), Number(nDn[2])), nDn[3]);
             return true;
         } else if (ndx !== null) {
-            this.rollAndCalc(ndx[0], this.roll_dx(Number(ndx[1]), (ndx[2] === undefined) ? 10 : Number(ndx[2].slice(1))), ndx[3]);
+            this.addText("roll " + ndx[0]);
+            this.calcCorrection(this.roll_dx(Number(ndx[1]), (ndx[2] === undefined) ? 10 : Number(ndx[2].slice(1))), ndx[3]);
+            return true;
+        } else if (nBn !== null) {
+            this.addText("roll " + nBn[0]);
+            this.roll_nBn(Number(nBn[1]), Number(nBn[2]), nBn[3], Number(nBn[4]));
             return true;
         } else if (D66_re.test(this.msg.content)) {
             this.addText("roll D66");
@@ -76,10 +126,15 @@ export class DiceRoller {
         }
     }
 
-    rollAndCalc(matched: string, result: number, correction: string | undefined) {
-        this.addText("roll " + matched);
+    calcCorrection(result: number, correction: string | undefined) {
         if (correction !== undefined) {
-            this.addText(`${result} + ${Number(correction)} = ${result + Number(correction)}`);
+            try {
+                const val = math.eval(correction.slice(1));
+                this.addText(`${result} + ${val} = ${result + val}`);
+            } catch (error) {
+                console.log("calcCorrection: " + String(error));
+                this.addText(`フォーマットが違います: ${correction}`);
+            }
         }
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,50 @@ import 'source-map-support/register';
 import Discord = require('discord.js');
 import readline from 'readline';
 import { DiceRoller } from './DiceRoller';
+import math from 'mathjs';
 
 var config = require('./config');
 
 const client = new Discord.Client();
+
+let env = {
+    otaku: false
+};
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', (msg: Discord.Message) => {
+    if (msg.author.bot) {
+        return;
+    }
+
     let dr = new DiceRoller(msg);
     if (dr.roll()) {
         console.log("DICE ROLL");
         dr.sendText();
+    }
+    
+    const compute_re = /^compute:(.+)$/i;
+    const compute = compute_re.exec(msg.content);
+    if (compute !== null) {
+        dr.addText(compute[0]);
+        try {
+            dr.addText(`${compute[1]} = ${math.eval(compute[1])}`);
+        } catch (error) {
+            console.log("compute error: " + String(error));
+            dr.addText(`フォーマットが違います: ${compute[1]}`);
+        }
+        console.log("COMPUTE " + compute[0]);
+        dr.sendText();
+    } 
+
+    const haibokusya = /..者/;
+    const result = haibokusya.exec(msg.content);
+    if (env.otaku && result !== null) {
+        console.log("ハァハァ");
+        msg.channel.send(`ハァ…ハァ… ${result[0]}……？`);
     }
 });
 
@@ -29,4 +59,11 @@ const rl = readline.createInterface({
 rl.on('close', () => {
     client.destroy();
     process.exit(0);
+});
+
+rl.on('line', (input) => {
+    if (input === "!toggle_otaku") {
+        env.otaku = !env.otaku;
+        console.log(`env.otaku = ${env.otaku}`);
+    }
 });
