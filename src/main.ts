bd641d1fe9adffbbf28546790b9d18a3518d1f9b
Yuya Kono <takohati0821@gmail.com>
2019-02-07 18:56:04 +0900

    頭がよくなった。

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import readline from 'readline';
 import { DiceRoller } from './DiceRoller';
 import math from 'mathjs';
 import { Messenger } from './Messenger';
+import kuromoji from 'kuromoji';
 
 var config = require('./config');
 
@@ -17,6 +18,18 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+let builder = kuromoji.builder({
+    dicPath: 'node_modules/kuromoji/dict'
+});
+let tokenizer: kuromoji.Tokenizer<kuromoji.IpadicFeatures>;
+builder.build((err, _tokenizer) => {
+    if (err) {
+        console.error(`${err}`);
+    } else {
+        tokenizer = _tokenizer;
+    }
+});
+
 client.on('message', (msg: Discord.Message) => {
     if (msg.author.bot) {
         return;
@@ -24,11 +37,13 @@ client.on('message', (msg: Discord.Message) => {
 
     let messenger = new Messenger(msg.channel);
 
+    // ダイスロール
     let dr = new DiceRoller(messenger);
     if (dr.roll(msg.content)) {
         console.log("DICE ROLL");
     }
 
+    // 計算
     const compute_re = /^compute:(.+)$/i;
     const compute = compute_re.exec(msg.content);
     if (compute !== null) {
@@ -42,11 +57,23 @@ client.on('message', (msg: Discord.Message) => {
         console.log("COMPUTE " + compute[0]);
     }
 
-    const haibokusya = /..者/;
-    const result = haibokusya.exec(msg.content);
-    if (env.otaku && result !== null) {
-        console.log("ハァハァ");
-        messenger.push(`ハァ…ハァ… ${result[0]}……？\n`);
+    // 敗北者
+    const haibokusya = /者$/;
+    if (env.otaku) {
+        let tokens = tokenizer.tokenize(msg.content);
+
+        let sya_index = tokens.findIndex(e => { return e.surface_form === "者" });
+        let syas = tokens.filter(e => haibokusya.test(e.surface_form));
+
+        if (sya_index > 0) {
+            if (tokens[sya_index - 1].pos === '名詞') {
+                console.log(`ハァハァ ${tokens[sya_index - 1].surface_form}`);
+                messenger.push(`ハァハァ…ハァハァ… ${tokens[sya_index - 1].surface_form}者……？\n`);
+            }
+        } else if (syas.length >= 1) {
+            console.log(`ハァハァ ${syas[0].surface_form}`);
+            messenger.push(`ハァハァ…ハァハァ… ${syas[0].surface_form}……？\n`);
+        }
     }
 
     messenger.send();
